8a851ff133e598ce58294688cd8c60e7b30afa72
younes reguieg <reguiegyounes@gmail.com>
2022-10-16 11:30:43 +0100

    Seeding In-Memory Database

---
+++ src/Services/Movies.API/Data/MovieSeed.cs
@@ -0,0 +1,57 @@
+﻿using Movies.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Movies.API.Data
+{
+    public static class MovieSeed
+    {
+        public static void SeedAsync(MovieDbContext movieDbContext) {
+            if (!movieDbContext.Movies.Any())
+            {
+                var movies = new List<Movie>
+                {
+                    new Movie {
+                        Id = 1,
+                        Genre = "Drama",
+                        Title = "The shawshank Redemption",
+                        Rating ="5.3",
+                        ImageUrl ="images/src",
+                        ReleaseTime=new DateTime(1994,5,5),
+                        Owner ="alice"
+                    },
+                    new Movie {
+                        Id = 2,
+                        Genre = "Crime",
+                        Title = "The Godfather",
+                        Rating ="9.2",
+                        ImageUrl ="images/src",
+                        ReleaseTime=new DateTime(1994,5,5),
+                        Owner ="alice"
+                    },
+                    new Movie {
+                        Id = 3,
+                        Genre = "Action",
+                        Title = "The Dark Knight",
+                        Rating ="9.1",
+                        ImageUrl ="images/src",
+                        ReleaseTime=new DateTime(1994,5,5),
+                        Owner ="bob"
+                    },
+                    new Movie {
+                        Id = 4,
+                        Genre = "Crime",
+                        Title = "12 Angry Men",
+                        Rating ="8.9",
+                        ImageUrl ="images/src",
+                        ReleaseTime=new DateTime(1994,5,5),
+                        Owner ="bob"
+                    },
+                };
+                movieDbContext.Movies.AddRange(movies);
+                movieDbContext.SaveChanges();
+            }
+        }
+    }
+}
--- src/Services/Movies.API/Models/Movie.cs
+++ src/Services/Movies.API/Models/Movie.cs
@@ -7,7 +7,7 @@ namespace Movies.API.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
-        public DateTime dateTime { get; set; }
+        public DateTime ReleaseTime { get; set; }
         public string Owner { get; set; }
         public string Rating { get; set; }
         public string ImageUrl { get; set; }
