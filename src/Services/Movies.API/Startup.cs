8c54d7df8f42e7510cde8b97ac06daeadc53671c
younes reguieg <reguiegyounes@gmail.com>
2022-10-16 10:22:56 +0100

    Configuration dbcontext of Movies

--- src/Services/Movies.API/Startup.cs
+++ src/Services/Movies.API/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Movies.API.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +30,11 @@ namespace Movies.API
         {
 
             services.AddControllers();
+
+            services.AddDbContext<MovieDbContext>(opt => {
+                opt.UseNpgsql(Configuration.GetConnectionString("MoviesConnection"));
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Movies.API", Version = "v1" });
