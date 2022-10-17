f69e4fa9b090439873d9da37f6a2816e506ed6f6
younes reguieg <reguiegyounes@gmail.com>
2022-10-17 10:12:14 +0100

    Claim Based Authentication with client_id Claim Restriction

---
+++ src/Services/Movies.API/Controllers/IdentityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Movies.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        public IActionResult Get() {
+            return new JsonResult(from c in User.Claims select new { c.Type ,c.Value});
+        }
+    }
+}
