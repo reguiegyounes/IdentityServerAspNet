ce6909478fc19cdce91d4ffa27edcc8b500f291a
younes reguieg <reguiegyounes@gmail.com>
2022-10-17 10:49:33 +0100

    Create Controller and Views for Movie Model

---
+++ WebApps/Movies.Client/Data/MoviesClientContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Client.Models;
+
+namespace Movies.Client.Data
+{
+    public class MoviesClientContext : DbContext
+    {
+        public MoviesClientContext (DbContextOptions<MoviesClientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
