8a981f44c2d89c2c60faeff4da608d0cd59b23f0
younes reguieg <reguiegyounes@gmail.com>
2022-10-18 10:30:26 +0100

    Developing Logout Button For Signout from IS4 and return to main page

---
+++ WebApps/Movies.Client/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Movies.Client.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        [Authorize]
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
