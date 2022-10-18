98344c7eb6571a9a555bb4d5a7c2f514c5070bd4
younes reguieg <reguiegyounes@gmail.com>
2022-10-18 12:03:32 +0100

    Consume Protected Movie.API with Getting Token from IS4

--- WebApps/Movies.Client/ApiServices/MovieApiService.cs
+++ WebApps/Movies.Client/ApiServices/MovieApiService.cs
@@ -1,28 +1,60 @@
-﻿using Movies.Client.Models;
+﻿using IdentityModel.Client;
+using Movies.Client.Models;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Movies.Client.ApiServices
 {
     public class MovieApiService : IMovieApiService
     {
+        private readonly IConfiguration _configuration;
+
+        public MovieApiService(IConfiguration  configuration)
+        {
+            _configuration=configuration;
+        }
         public async Task<IEnumerable<Movie>> GetAllMovies()
         {
-            var movies = new List<Movie>
-                {
-                    new Movie {
-                        Id = 1,
-                        Genre = "Drama",
-                        Title = "The shawshank Redemption",
-                        Rating ="5.3",
-                        ImageUrl ="images/src",
-                        ReleaseTime=new DateTime(1994,5,5),
-                        Owner ="alice"
-                    }
+            // "retrieve" our api credetials . This must be registred on Identity Server
+            var apiClientCredentials = new ClientCredentialsTokenRequest
+            {
+                Address=$"{_configuration["oidc:authority_url"]}/connect/token",
+                ClientId=_configuration["Movies.API:ClientId"],
+                ClientSecret=_configuration["Movies.API:ClientSecret"],
+                Scope= _configuration["Movies.API:Scope"]
             };
+            
+            var client = new HttpClient();
 
-            return await Task.FromResult(movies);
+            var disco = await client.GetDiscoveryDocumentAsync(_configuration["oidc:authority_url"]);
+            if (disco.IsError)
+            {
+                return null; // throw 500 error
+            }
+            // get an access token 
+            var tokenResponse=await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            if (tokenResponse.IsError)
+            {
+                return null;
+            }
+
+
+            // Send request to protected api
+
+            var apiClient=new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            var response = await apiClient.GetAsync($"{_configuration["Movies.API:Url"]}/api/movies");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            List<Movie> moviesList =  JsonConvert.DeserializeObject<List<Movie>>(content);
+
+            return moviesList;
+            
         }
 
         public Task<IEnumerable<Movie>> GetMovieById(int id)
